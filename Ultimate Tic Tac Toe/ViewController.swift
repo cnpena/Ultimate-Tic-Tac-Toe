92002b9457fe05ea2af2cd6c00c519b3007b340f
Celine Pena <cnpena@ucsc.edu>
2017-08-26 13:01:59 -0700

    Initial Commit

---
+++ Ultimate Tic Tac Toe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ultimate Tic Tac Toe
+//
+//  Created by Celine Pena on 8/26/17.
+//  Copyright © 2017 Celine Pena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
