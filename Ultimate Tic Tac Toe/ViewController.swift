e9a1165f9905906614bc708ab55fafc48cb4642a
Celine Pena <cnpena@ucsc.edu>
2017-08-26 19:57:15 -0700

    initial commit of code that sets up board UI and allows the user to place only "x"s on the board

--- Ultimate Tic Tac Toe/ViewController.swift
+++ Ultimate Tic Tac Toe/ViewController.swift
@@ -8,16 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    @IBOutlet weak var board: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.board.dataSource = self
+        self.board.delegate = self
+        board.allowsMultipleSelection = true
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 81
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "board", for: indexPath) as! boardCell
+        return cell
+    }
+    
+    //UI: Collection View function that determines the size of the user cards
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: IndexPath) -> CGSize {
+        let cellWidth = view.bounds.size.width/9
+        return CGSize(width: cellWidth, height: cellWidth)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath) as! boardCell
+        cell.marker.image = #imageLiteral(resourceName: "x")
     }
 
 
---
+++ Ultimate Tic Tac Toe/boardCell.swift
@@ -0,0 +1,13 @@
+//
+//  boardCell.swift
+//  Ultimate Tic Tac Toe
+//
+//  Created by Celine Pena on 8/26/17.
+//  Copyright © 2017 Celine Pena. All rights reserved.
+//
+
+import UIKit
+
+class boardCell: UICollectionViewCell {
+    @IBOutlet weak var marker: UIImageView!
+}
